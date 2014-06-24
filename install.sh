8f08bcd3c7da0a9e411428d1e0a3d55df645c5f9
Volmer <git@radicaos.com>
2014-06-24 12:45:27 -0400

    Symlink dotfiles project during install

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Symlinking $PWD..."
+rm -f ~/.dotfiles
+ln -s $PWD ~/.dotfiles
+
 for file in ~/.dotfiles/dotfiles/*; do
   echo "Installing $(basename $file)..."
-  rm -r ~/.$(basename $file)
+  rm ~/.$(basename $file)
   ln -s $file ~/.$(basename $file)
 done
