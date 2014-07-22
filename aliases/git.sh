5e21bba7ff91cbddf9ecfc6ac296d3c4517434c7
Volmer <git@radicaos.com>
2014-07-22 16:11:25 -0400

    Post commit image

--- aliases/git.sh
+++ aliases/git.sh
@@ -10,4 +10,4 @@ alias gco="g checkout"
 alias gl="g log"
 alias gt="g log --graph --decorate --oneline --all"
 alias gme='g config --local user.name "Volmer Soares" && git config --local user.email volmerius@gmail.com'
-alias commit-image='ln -s ~/.bin/post-commit-image .git/hooks/post-commit'
+alias commit-image='ln -s ~/.dotfiles/bin/post-commit-image .git/hooks/post-commit'
