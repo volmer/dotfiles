0939659018f16e93b5fa5ce482fba0ca81b9e57b
Volmer <git@radicaos.com>
2015-04-19 11:35:44 -0400

    Sublime Text 3 preferences and packages

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 echo "Symlinking $PWD..."
-rm -f ~/.dotfiles
-ln -s $PWD ~/.dotfiles
+ln -s -f $PWD ~/.dotfiles
 
 for file in ~/.dotfiles/dotfiles/*; do
   echo "Installing $(basename $file)..."
-  rm ~/.$(basename $file)
-  ln -s $file ~/.$(basename $file)
+  ln -s -f $file ~/.$(basename $file)
 done
+
+if type "subl" > /dev/null 2>&1 ; then
+  echo "Installing Sublime Text 3 user prefrences..."
+  ln -s -i ~/.dotfiles/Sublime\ Text\ 3/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+fi
