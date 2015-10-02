193109ceb0395f92cdc6252c6d1a3986cdd57e70
Volmer <git@radicaos.com>
2015-10-02 10:20:41 +0200

    Giving up on iTerm

--- install.sh
+++ install.sh
@@ -12,9 +12,3 @@ if type "subl" > /dev/null 2>&1 ; then
   echo "Installing Sublime Text 3 user prefrences..."
   ln -s -i ~/.dotfiles/Sublime\ Text\ 3/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
 fi
-
-if type "defaults" > /dev/null 2>&1 ; then
-  echo "Installing iTerm prefrences..."
-  defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder 1
-  defaults write com.googlecode.iterm2.plist PrefsCustomFolder '~/.dotfiles/iTerm'
-fi
