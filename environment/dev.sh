14e7530ae18ab09a25dcbf35c7899f421660dd28
Volmer Campos Soares <git@radicaos.com>
2017-10-07 14:27:44 -0400

    Update dotfiles

---
+++ environment/dev.sh
@@ -0,0 +1,6 @@
+# load dev, but only if present and the shell is interactive
+if [[ -f /opt/dev/dev.sh ]] && [[ $- == *i* ]]; then
+  source /opt/dev/dev.sh
+fi
+
+export PATH="$HOME/.yarn/bin:$PATH"
