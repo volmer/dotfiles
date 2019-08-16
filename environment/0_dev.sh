245009aef379b5405a08a75e562441ebac7b3f92
Volmer Campos Soares <git@radicaos.com>
2019-08-16 10:06:31 -0400

    Add Nix required by Dev

--- environment/0_dev.sh
+++ environment/0_dev.sh
@@ -2,3 +2,5 @@
 if [[ -f /opt/dev/dev.sh ]] && [[ $- == *i* ]]; then
   source /opt/dev/dev.sh
 fi
+
+if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then . ~/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
