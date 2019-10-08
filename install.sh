e329c63c4d6dc76f3bfaddb67b2598d91344145e
Volmer Campos Soares <git@radicaos.com>
2019-10-08 11:01:02 -0400

    Remove Atom

--- install.sh
+++ install.sh
@@ -11,17 +11,3 @@ for file in ~/.dotfiles/dotfiles/*; do
   echo "Installing $(basename $file)..."
   ln -s -f $file ~/.$(basename $file)
 done
-
-if type "atom" > /dev/null 2>&1 ; then
-  echo "Installing Atom prefrences..."
-  for file in ~/.dotfiles/atom/*; do
-    echo "Installing $(basename $file)..."
-    ln -s -f $file ~/.atom/$(basename $file)
-  done
-
-  echo "Installing Atom packages..."
-  # To generate: apm list --installed --bare > ~/.dotfiles/atom/packages.txt
-  apm install --packages-file ~/.dotfiles/atom/packages.txt
-else
-  echo "Atom not found! Install Atom and re-run this command."
-fi
