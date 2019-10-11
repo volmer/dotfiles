45af7853575fd6f4101b2a12a167b05f8ec01137
Volmer Campos Soares <git@radicaos.com>
2019-10-11 15:35:26 -0400

    Use Visual Studio Code

--- environment/editor.sh
+++ environment/editor.sh
@@ -1,1 +1,1 @@
-
+export EDITOR="code --wait"
--- install.sh
+++ install.sh
@@ -11,3 +11,13 @@ for file in ~/.dotfiles/dotfiles/*; do
   echo "Installing $(basename $file)..."
   ln -s -f $file ~/.$(basename $file)
 done
+
+if type "code" > /dev/null 2>&1 ; then
+   echo "Installing Visual Studio Code prefrences..."
+   for file in ~/.dotfiles/code/*; do
+     echo "Installing $(basename $file)..."
+     ln -s -f $file ~/Library/Application\ Support/Code/User/$(basename $file)
+   done
+ else
+   echo "Visual Studio Code not found! Install it and re-run this command."
+ fi
