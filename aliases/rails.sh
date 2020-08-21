77f38cb3f6c76734547f61e283776738adc856a3
Volmer Campos Soares <git@radicaos.com>
2020-08-21 16:23:29 -0400

    Start Rails server with SSL

--- aliases/rails.sh
+++ aliases/rails.sh
@@ -1,4 +1,4 @@
 alias rc="bin/rails console"
-alias sr="bin/rails server"
+alias sr="bin/rails server -b 'ssl://127.0.0.1:3000?key=/Users/volmer/rails.local.key.pem&cert=/Users/volmer/rails.local.pem'"
 alias rt="bin/rails test"
 alias rst="bin/rails test:system"
